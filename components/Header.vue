fc9182df76ca8d5300d59ddc9cd1de9414c305bb
benjamingeets <benjamingeets@tutamail.com>
2021-05-16 14:35:16 +0200

    edit "suivre" button

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
         <h1 class="md:text-4xl sm:text-3xl text-3xl lg:text-left text-center font-bold">Benjamin Geets</h1>
         <div class="flex justify-between pt-5 items-center">
             <Navbar/>
-            <a target="_blank" href="/feed.xml" class="border-2 px-8 py-2 rounded-md items-center hover:bg-gray-200 duration-300 lg:flex hidden"><RSS color="#2c3e50" :size="iconSize"/>Suivre</a>
+            <a target="_blank" href="/feed.xml" class="border-2 px-8 py-2 rounded-md items-center hover:bg-gray-100 duration-300 lg:flex hidden"><RSS class="mr-2" color="#2c3e50" :size="iconSize"/>Suivre</a>
         </div>
     </header>
 </template>
