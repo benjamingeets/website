36a12b1e53815e1674e8b2bc58d46d00d00176a7
Benjamin Geets <benjamingeets@tutamail.com>
2021-11-03 22:11:25 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,8 @@
-
 <script>
     import ExternalLink from '$lib/icons/ExternalLink.svelte';
     import IconDisplay from '$lib/icons/IconDisplay.svelte'
     const title = "Benjamin Geets :: Développeur web (Tournai \ Belgique)"
-    const description = "Etudiant en e-Business depuis 2019, je me suis spécialisé dans le développement web à l'aide de frameworks tels Vue.js, Svelte,..."
+    const description = "Développeur web frontend: Jamstack, SvelteKit, Node.js, ..."
     const url = "https://geets.dev"
     const shareImage = url + "/images/share.jpg"
     let links = [
@@ -38,12 +37,12 @@
 
 
 <div class="min-h-screen w-screen dark:bg-gray-900 dark:text-gray-100">
-    <main class="max-w-5xl mx-auto min-h-screen flex items-center sm:p-4">
-        <div class="bg-gray-200 dark:bg-gray-800 p-8 sm:rounded-lg sm:min-h-0 min-h-screen flex md:flex-row flex-col md:justify-between">
+    <main class="max-w-3xl mx-auto min-h-screen flex items-center sm:p-4">
+        <div class="w-full bg-gray-200 dark:bg-gray-800 p-8 sm:rounded-lg sm:min-h-0 min-h-screen flex md:flex-row flex-col md:justify-between">
             <div class="flex flex-col md:w-9/12 w-12/12">
                 <h1 class="md:text-5xl text-3xl font-bold">Benjamin Geets 🍃</h1>
                 <h2 class="font-bold md:text-2xl text-lg my-2">Développeur web</h2>
-                <p>Etudiant en dernière année à la <b>HEPH Condorcet</b> en section e-Business, j'ai toujours été passionné par l'informatique. J'ai commencé à bidouiller <b>dès que j'ai eu un ordinateur entre les mains</b>. Aujourd'hui, je me suis beaucoup <b>formé à travers différents cours en ligne</b> et développe principalement en JavaScript avec Svelte, Vue, React,... et je commence à me former au backend avec <b>Node.js</b>.</p>
+                <p>#SvelteKit #Nodejs #Tailwind #Jamstack <small>#Vue #React</small> </p>
                 
             </div>
             <hr class="md:hidden block my-4 h-1">
