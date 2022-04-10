2eff54c8c13608cd186b997d1832ff625ef05668
benjamingeets <benjamingeets@tutamail.com>
2022-04-10 17:31:14 +0200

    feat: icon & links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main': 'Outfit'
+      },
+      colors: {
+        dark: '#2b2b2b'
+      },
+    },
   },
   plugins: [],
 }
