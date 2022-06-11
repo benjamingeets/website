d392e0bca6b1225122fa63fec40fa78d348ffe91
Benjamin Geets <45077541+benjamingeets@users.noreply.github.com>
2022-06-11 15:27:26 +0200

    feat: use deno blog

---
+++ main.js
@@ -0,0 +1,14 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "Benjamin Geets",
+  author: "Benjamin Geets",
+  avatar: "./frog.png",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "mailto:benjamin@loak.studio" },
+    { title: "GitHub", url: "https://github.com/benjamingeets" },
+    { title: "LinkedIn", url: "https://linkedin.com/in/benjamingeets" },
+  ],
+  background: "#f9f9f9"
+});
